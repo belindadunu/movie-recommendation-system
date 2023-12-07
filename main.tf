c7944ebaeb413a7c98a47fee77e5217debe66902
belindadunu <belindadunu24@gmail.com>
2023-12-07 16:23:33 +0000

    test

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
 # The configuration for the `remote` backend
 terraform {
-  backend "remote" {
-    organization = "C4_Final_Proj"
-
-    workspaces {
-      name = "default"
-    }
-  }
-}
-
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
+  backend "local" {}
 }
