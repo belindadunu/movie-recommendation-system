01cffa3aab17c3df93fca1008489db32dc711090
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-12-13 13:46:35 -0500

    Update settings.py

--- backend/movie_recommendation/settings.py
+++ backend/movie_recommendation/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='empty')
 DEBUG = True  # Remember to set this to False in production
 
 
-ALLOWED_HOSTS = ['backend', 'frontend']
+ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
