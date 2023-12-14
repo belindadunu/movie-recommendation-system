4976894b85e690945c47664dbe81954cae746a95
Belinda Dunu <139175163+belindadunu@users.noreply.github.com>
2023-12-14 14:24:06 +0000

    rm terraform org

--- backend.tf
+++ backend.tf
@@ -1,16 +1,6 @@
 # based on https://developer.hashicorp.com/terraform/language/settings/backends/configuration
 
 terraform {
-  cloud {
-    organization = "C4_Final_Proj"
-
-    workspaces {
-      name = "c4-final-proj"
-    }
-  }
-}
-
-terraform {
   backend "s3" {
     bucket = "kura-c4-final-proj"
     key    = "kura-c4-final-proj/terraform/terraform.tfstate"
