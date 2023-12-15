b235d2641f4124c718822929151e24e22197067d
Belinda Dunu <139175163+belindadunu@users.noreply.github.com>
2023-12-15 18:59:40 -0500

    Create cloudwatch-alarms.tf

---
+++ alertsTerraform/cloudwatch-alarms.tf
@@ -0,0 +1,17 @@
+# based on https://registry.terraform.io/modules/clouddrove/cloudwatch-alarms/aws/latest
+# and https://github.com/lorenzoaiello/terraform-aws-rds-alarms
+
+
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization_high" {
+  alarm_name          = "highCPUUtilization"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/Logs"
+  period              = "600"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "Average database CPU utilization over last 10 minutes too high"
+  alarm_actions       = [resource.aws_sns_topic.error_topic.arn]
+
+}​
