069a62c8b7c26baa694127008ea8a969de708773
Belinda Dunu <139175163+belindadunu@users.noreply.github.com>
2023-12-16 08:25:25 -0500

    Update sns-topic.tf

--- alertsTerraform/sns-topic.tf
+++ alertsTerraform/sns-topic.tf
@@ -1,7 +1,9 @@
-resource "aws_sns_topic" "error_topic" {
+# Based on https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_topic
+resource "aws_sns_topic" "error_topic" { # "message "
   name = "errorTest"
 }
 
+# Based on https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_topic_subscription
 resource "aws_sns_topic_subscription" "error_email_subscription" {
   topic_arn = aws_sns_topic.error_topic_test.arn
   protocol  = "email"
