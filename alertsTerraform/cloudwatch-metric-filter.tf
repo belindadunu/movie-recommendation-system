b1d8f0ffaf945e675489361df8b7be38fd5d8b26
Belinda Dunu <139175163+belindadunu@users.noreply.github.com>
2023-12-16 10:13:33 -0500

    Update cloudwatch-metric-filter.tf

--- alertsTerraform/cloudwatch-metric-filter.tf
+++ alertsTerraform/cloudwatch-metric-filter.tf
@@ -6,7 +6,6 @@ resource "aws_cloudwatch_log_metric_filter" "d10_final" {
   log_group_name = aws_cloudwatch_log_group.d10_final.name
 
   metric_transformation {
-    name      = "EventCount"
     namespace = "AWS/Logs"
     value     = "1"
   }
