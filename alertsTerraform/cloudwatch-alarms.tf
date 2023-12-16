1d5820578ea66d7bfb9133e30b68fd0ac0d5bd8a
Belinda Dunu <139175163+belindadunu@users.noreply.github.com>
2023-12-16 08:12:51 -0500

    Update cloudwatch-alarms.tf

--- alertsTerraform/cloudwatch-alarms.tf
+++ alertsTerraform/cloudwatch-alarms.tf
@@ -1,8 +1,6 @@
 # based on https://registry.terraform.io/modules/clouddrove/cloudwatch-alarms/aws/latest
-# and https://github.com/lorenzoaiello/terraform-aws-rds-alarms
 
-# over 70%
-
+# Alarm for when CPU utilization is over 70% after a period of 10 minutes
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization_high" {
   alarm_name          = "highCPUUtilization"
   comparison_operator = "GreaterThanThreshold"
@@ -11,7 +9,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_high" {
   namespace           = "AWS/Logs"
   period              = "600"
   statistic           = "Average"
-  threshold           = "80"
+  threshold           = "70"
   alarm_description   = "Average database CPU utilization over last 10 minutes too high"
   alarm_actions       = [resource.aws_sns_topic.error_topic.arn]
 
